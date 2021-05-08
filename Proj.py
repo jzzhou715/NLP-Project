5b5aa87e690f3ac74599b59faec23c90969ee18e
jzzhou715 <61242291+jzzhou715@users.noreply.github.com>
2021-05-08 16:43:22 -0400

    Update Proj.py

--- Proj.py
+++ Proj.py
@@ -3,6 +3,11 @@
 import pandas as pd
 import matplotlib as plt
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+# =============================================================================
+# Clean
+# =============================================================================
 
 data = pd.read_csv('yelp.csv')
 
@@ -17,4 +22,23 @@ data['popular'] = (data['reaction'] > popular_threshold)
 data_label = data[['text', 'cool', 'useful', 'funny', 'reaction', 'popular']]
 
 data_X = data['text']
+
+# =============================================================================
+# Vectorize nlp
+# =============================================================================
+
+Vec = CountVectorizer(input='filename',
+                      analyzer = 'word',
+                      stop_words='english',
+                      lowercase = True)
+
+Vec_Bi = CountVectorizer(input='filename',
+                         analyzer = 'word',
+                         stop_words='english',
+                         lowercase = True,
+                         binary=True)
+
+review_vec = pd.DataFrame()
+
+path = '/Users/Zhou/Box/GitHub Repository/NLP-Project/yelp.csv'
 
